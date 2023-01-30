c80c21efa8c4d552b50472c2492e95f679df831e
Justin Herndon <jherndon111@gmail.com>
2023-01-30 10:49:24 -0600

    added comments into the files

--- pa1/jh4411_exec.c
+++ pa1/jh4411_exec.c
@@ -2,23 +2,27 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
-
+// This is a check to make sure that the currect amount of arguemnts are used for the executable
 if(argc < 4){
 printf("Not enough arguemnts were entered into the command line. Please enter the correct amount\n");
 return 1;
 }
-
+// Setting arg 1 as the command, since arg 0 is the executable name. Another counter is initalized
+// becuase the command starts at arg 2 for the inputted executable while it needs to be in 
+// position 0 for the arguements array.
+// Also setting up the arg array as 50 args as stated in the instructions. 
 char* command = argv[1];
 char* arguements[50] = {};
 int counter = 0;
-
+// Simple for loop to place the inputted arguements in the correct place in the arguemnts array. It 
+// runs until the given args to the executable NULL thanks to the argc variable.
 for(int i=2;i<argc;i++){
 arguements[counter] = argv[i];
 counter++;
 }
 
 int status_code = execvp(command,arguements);
-
+// error checking to make sure that the command actually ran and is a valid linux/unix command
 if(status_code == -1){
 printf("The command and/or arguements that were inputted are not valid\n");
 return 1;
--- pa1/jh4411_fork.c
+++ pa1/jh4411_fork.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 
 int main(){
-
+//Not much to explain here. This is a for loop that forks on every instance until completion and
+//prints out its current counter value
 for(int i=0; i<20;i++){
 fork();
 printf("%d",i);
