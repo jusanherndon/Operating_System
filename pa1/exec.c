96b20b115ca7ecb7626996a3c0aa9b1714a0141a
Justin Herndon <jherndon111@gmail.com>
2023-01-29 19:00:31 -0600

    removing unnecessary print statement from my exec code

--- pa1/exec.c
+++ pa1/exec.c
@@ -5,13 +5,10 @@ int main(int argc, char *argv[]){
 
 char* command = argv[1];
 char* arguements[50] = {};
-
-printf("%s\n",command);
-
 int counter = 0;
+
 for(int i=2;i<argc;i++){
 arguements[counter] = argv[i];
-printf("\n%s\n",arguements[counter]);
 counter++;
 }
 
