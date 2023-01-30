12dca44c7bc0035b0510e70694966bc3755ebb21
Justin Herndon <jherndon111@gmail.com>
2023-01-29 18:35:34 -0600

    got execvp to run correctly

--- pa1/exec.c
+++ pa1/exec.c
@@ -4,13 +4,23 @@
 int main(int argc, char *argv[]){
 
 char* command = argv[1];
-char* arguements[50];
+char* arguements[50] = {};
 
 printf("%s\n",command);
 
+int counter = 0;
 for(int i=2;i<argc;i++){
-arguements[i] = argv[i];
-printf("\n%s\n",arguements[i]);
+arguements[counter] = argv[i];
+printf("\n%s\n",arguements[counter]);
+counter++;
 }
 
+int status_code = execvp(command,arguements);
+
+if(status_code == -1){
+printf("Process did not terminate corectly\n");
+return 1;
+}
+
+return 0;
 }
