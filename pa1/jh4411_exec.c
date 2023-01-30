720370cef3d6f1c9301a0e2c28e58cd8ae8560de
Justin Herndon <jherndon111@gmail.com>
2023-01-29 19:22:25 -0600

    making the code look cleaner and with less lines

--- pa1/jh4411_exec.c
+++ pa1/jh4411_exec.c
@@ -11,12 +11,9 @@ return 1;
 char* command = argv[1];
 char* arguements[50] = {};
 int counter = 0;
-int exit_code = 0;
-int status_code = 0;
 
 for(int i=2;i<4;i++){
 if(argv[i]== NULL){
-exit_code = -1; 
 printf("An arguement was left out. Please rerun the program again with all of its arguements\n");
 return 1;
 }
@@ -24,9 +21,7 @@ arguements[counter] = argv[i];
 counter++;
 }
 
-if(exit_code != -1){
-status_code = execvp(command,arguements);
-}
+int status_code = execvp(command,arguements);
 
 if(status_code == -1){
 printf("Process did not terminate corectly\n");
