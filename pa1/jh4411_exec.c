fc3e307afb6434f6c1ccd2831d4ccd7ed4ddc31d
Justin Herndon <jherndon111@gmail.com>
2023-01-29 19:18:55 -0600

    added error checking into my exec.c file

--- pa1/jh4411_exec.c
+++ pa1/jh4411_exec.c
@@ -3,16 +3,30 @@
 
 int main(int argc, char *argv[]){
 
+if(argv[1]== NULL){
+printf("A command was not entered in as an arguement. Rerun the program.\n");
+return 1;
+}
+
 char* command = argv[1];
 char* arguements[50] = {};
 int counter = 0;
+int exit_code = 0;
+int status_code = 0;
 
-for(int i=2;i<argc;i++){
+for(int i=2;i<4;i++){
+if(argv[i]== NULL){
+exit_code = -1; 
+printf("An arguement was left out. Please rerun the program again with all of its arguements\n");
+return 1;
+}
 arguements[counter] = argv[i];
 counter++;
 }
 
-int status_code = execvp(command,arguements);
+if(exit_code != -1){
+status_code = execvp(command,arguements);
+}
 
 if(status_code == -1){
 printf("Process did not terminate corectly\n");
