93cecc843d3a63b63a8ec8911e784b2c2788168d
Justin Herndon <jherndon111@gmail.com>
2023-02-16 17:14:35 -0600

    working on R1 with using https://www.w3schools.com/c/c_user_input.php as my guide

--- pa2/jh4411_shell.c
+++ pa2/jh4411_shell.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
+int main(){
+// logic used in my last programming assignment in order to take 
+// in command arguements and run it as a seperate process this time
+
+
+char input[10];
+
+while(1){
+
+printf("Please enter a string into the terminal:");
+scanf("%s",input);
+
+printf("\n%s\n", input);
+}
+
+/*
+char* command = argv[1];
+char* arguements[50] = {};
+int counter = 0;
+
+for(int i=2;i<argc;i++){
+arguements[counter] = argv[i];
+counter++;
+}
+
+fork();
+int status_code = execvp(command,arguements);
+
+if(status_code == -1){
+printf("The command and/or arguements that were inputted are not valid\n");
+return 1;
+}
+*/
+
+
+
 
 
 return 0;
