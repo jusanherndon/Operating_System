ebd1d443617a2da8bdf22f0447d2c11cc034ec4f
Justin Herndon <jherndon111@gmail.com>
2023-02-20 16:58:41 -0600

    The program can read input and kinda work

--- pa2/jh4411_shell.c
+++ pa2/jh4411_shell.c
@@ -2,47 +2,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 //I used babyshell and simple-strtok as examples
 
 int main(){
 
-char input[50];
+char input[257];
 char* args[50] = {};
 char* arguements[50] = {};
+int pid;
 int counter = 0;
 int count = 0;
+int status_code; 
 
 while(1){
 
-printf("Please enter a string into the terminal:");
-while ( fgets(input,256,stdin) != NULL ){	
-    char* pch = strtok (input," \t\n");
-    while (pch != NULL)
-        {
-	    args[counter] = pch;
-	    pch = strtok (NULL, " \t\n");
-	    counter++;
-        }    
-    for(int i=1; i<counter; i++)
-        {
-	    arguements[count] = args[i];
-	    count++;
-        }  
-    char* command = args[0]; 
-
-    int status_code = execvp(command,arguements);
-    
-    // error cheching on the command being run
-    if(status_code == -1){
-        printf("The command and/or arguements that were inputted are not valid\n");
-        return 1;
+    printf("Please enter a string into the terminal:");
+    while ( fgets(input,256,stdin) != NULL ){	
+        char* pch = strtok (input," \t\n");
+        while (pch != NULL)
+            {
+	        args[counter] = pch;
+	        pch = strtok (NULL, " \t\n");
+	        counter++;
+            }    
+        char* command = args[0]; 
+        switch(pid=fork()){
+            /*
+            default: wait();
+	         break;
+            */
+	    case 0: status_code = execvp(command,args);
+		    break;
+        }
     }
-//execlp(command,command, (char *) NULL);  
-
 }
-}
-
 
 return 0;
 }
