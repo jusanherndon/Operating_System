b8df6fccf7f435191727bc707ecdff76faddaf0e
Justin Herndon <jherndon111@gmail.com>
2023-02-20 17:51:16 -0600

    working on R2, I think R1 is done

--- pa2/jh4411_shell.c
+++ pa2/jh4411_shell.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 //I used babyshell and simple-strtok as examples
 
@@ -12,9 +14,9 @@ char input[257];
 char* args[50] = {};
 char* arguements[50] = {};
 int pid;
+int ampersant = 0;
 int counter = 0;
-int count = 0;
-int status_code; 
+int status_code = 0; 
 
 while(1){
 
@@ -28,15 +30,27 @@ while(1){
 	        counter++;
             }    
         char* command = args[0]; 
+
+	for(int i = 1; i <counter; i++){
+            if(strcspn(args[i], "&")){
+	        ampersant = 1;
+	    }
+	}
+
         switch(pid=fork()){
-            /*
-            default: wait();
-	         break;
-            */
+	    /*	    
+            default: 
+	             break;
+	    */
 	    case 0: status_code = execvp(command,args);
+		    wait(&status_code);
+		    break;
+	    case 1: status_code = execvp(command,args);
+		    wait(&status_code);
 		    break;
         }
     }
+    printf("Please enter a string into the terminal:");
 }
 
 return 0;
