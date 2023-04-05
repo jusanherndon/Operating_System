a6ff8be972a61978ddd4a10266f4f0516ff611dc
Justin Herndon <jherndon111@gmail.com>
2023-04-05 17:09:30 -0500

    adding functionality to the code

--- pa3/jh4411_linecount.c
+++ pa3/jh4411_linecount.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <pthread.h>
 
-int main() {
+int filelinecount(char filename[]){
+FILE* file;
+int lines;
+char buf[200];
 
+file = fopen(filename, "r"); 
 
+while(fgets(buf,sizeof(buf),file) != NULL)
+{
+  lines++;
+}
+
+return lines;
+}
+
+int main(int argc, char* argv[]) {
+int linecount = 0;
+int lines;
+
+for(int i = 1; i <argc; i++){
+     lines = filelinecount(argv[i]);
+     printf("\n%d %s\n", lines, argv[i]);
+     linecount += lines;
+     lines = 0;
+}
+
+printf("%d total\n", linecount);
 }
