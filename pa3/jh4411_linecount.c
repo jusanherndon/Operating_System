e95005b68a3a43a0dc18e750bcf8b7c16c797897
Justin Herndon <jherndon111@gmail.com>
2023-04-05 21:22:27 -0500

    almost got the program working, I looked at https://www.tutorialspoint.com/multithreading-in-c

--- pa3/jh4411_linecount.c
+++ pa3/jh4411_linecount.c
@@ -1,35 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
+#include <unistd.h>
 
-int filelinecount(char filename[]){
-FILE* file;
 int lines = 0;
+
+void* filelinecount(void * arg){
+FILE* file;
 
 char *line = NULL;
 size_t len = 0;
 ssize_t read;
 
-file = fopen(filename, "r"); 
+file = fopen(arg, "r"); 
 
 while((read = getline(&line, &len, file)) != -1){
      lines++;
 }
 
+printf("%d %s\n", lines, arg);
+
 fclose(file);
-return lines;
+lines = 0;
+
 }
 
 int main(int argc, char* argv[]) {
+int error;
+pthread_t tid[argc];
 int linecount = 0;
-int lines;
+int lines = 0;
+
+
 
 for(int i = 1; i <argc; i++){
-     lines = filelinecount(argv[i]);
-     printf("%d %s\n", lines, argv[i]);
+     error = pthread_create(&tid[i-1], NULL, &filelinecount, (char**)argv[i]);
+     if (error != 0) printf("\nThread can't be created : [%s]", strerror(error));
+     lines = error;
+     pthread_join(tid[i-1], NULL);
      linecount += lines;
      lines = 0;
 }
+
 
 printf("%d total\n", linecount);
 }
