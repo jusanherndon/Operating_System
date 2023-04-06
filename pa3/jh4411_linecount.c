0dce17eafd0bcfeb336a451e3d5a962f8e38d856
Justin Herndon <jherndon111@gmail.com>
2023-04-05 21:31:34 -0500

    trying to implement mutex locks on the variables

--- pa3/jh4411_linecount.c
+++ pa3/jh4411_linecount.c
@@ -4,9 +4,12 @@
 #include <string.h>
 #include <unistd.h>
 
+pthread_mutex_t lock;
 int lines = 0;
+int linecount = 0;
 
 void* filelinecount(void * arg){
+pthread_mutex_lock(&lock);
 FILE* file;
 
 char *line = NULL;
@@ -23,26 +26,26 @@ printf("%d %s\n", lines, arg);
 
 fclose(file);
 lines = 0;
-
+linecount += lines;
+pthread_mutex_unlock(&lock);
 }
 
 int main(int argc, char* argv[]) {
 int error;
 pthread_t tid[argc];
-int linecount = 0;
-int lines = 0;
 
-
+if (pthread_mutex_init(&lock, NULL) != 0) {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
 
 for(int i = 1; i <argc; i++){
      error = pthread_create(&tid[i-1], NULL, &filelinecount, (char**)argv[i]);
      if (error != 0) printf("\nThread can't be created : [%s]", strerror(error));
-     lines = error;
      pthread_join(tid[i-1], NULL);
-     linecount += lines;
-     lines = 0;
 }
 
-
 printf("%d total\n", linecount);
+pthread_mutex_destroy(&lock);
+return 0;
 }
